3e8ea372a6d5ec7823cef23584ec67990219c4e6
Pim Otte <potte@quintor.nl>
2019-01-29 11:47:25 +0100

    Introduce non-system tests

--- waardepapieren-service/src/index.js
+++ waardepapieren-service/src/index.js
@@ -14,11 +14,8 @@ class WaardenpapierenService {
 
     const nlxConnector = await core.getConnector('nlx')
     nlxConnector.configure(nlxOutwayEndpoint)
-    console.log("2.5")
     let ssid = await abundance.attendTo('ephemeral', BRP_UITTREKSEL)
-    console.log("2.55")
     let observer = await abundance.observe(ssid.did, 'ephemeral')
-    console.log("2.56")
     observer.subscribe(async (needClaim) => {
       console.log('Someone is in need!')
       await this.serveNeed(ssid, needClaim)
@@ -30,34 +27,33 @@ class WaardenpapierenService {
       }
     })
 
-    console.log("2.6")
     return ssid
   }
 
   async serveNeed (serviceSsid, need) {
     const core = abundance.getCoreAPI()
 
-    let did = 'did:discipl:ephemeral:'+need.ssid.pubkey
+    let did = 'did:discipl:ephemeral:' + need.ssid.pubkey
 
-    console.log('waiting for BSN of ssid: '+did)
+    console.log('waiting for BSN of ssid: ' + did)
 
-    const observer = await core.observe({did:did}, { [BSN_CLAIM_PREDICATE]: null })
+    const observer = await core.observe({ did: did }, { [BSN_CLAIM_PREDICATE]: null })
 
     observer.subscribe(async (bsnClaim) => {
       const bsn = bsnClaim['claim']['data'][BSN_CLAIM_PREDICATE]
-      console.log('service: BSN info found :'+bsn)
+      console.log('service: BSN info found :' + bsn)
       const nlxConnector = await core.getConnector('nlx')
-      //let identifier = await nlxConnector.claim(null, { 'path': '/haarlem/Basisregistratiepersonen/RaadpleegIngeschrevenPersoonNAW', 'params': { 'burgerservicenummer': bsn } })
+      let identifier = await nlxConnector.claim(null, { 'path': '/haarlem/Basisregistratiepersonen/RaadpleegIngeschrevenPersoonNAW', 'params': { 'burgerservicenummer': bsn } })
 
-      let result = {woonplaats:'dummy value'}//await nlxConnector.get(identifier)
+      let result = await nlxConnector.get(identifier)
 
-      console.log('NLX result value: '+result.value)
+      console.log('NLX result value: ' + result.value)
       let personalSsid = await core.newSsid('ephemeral')
-      console.log('created private channel : '+JSON.stringify(personalSsid))
+      console.log('created private channel : ' + JSON.stringify(personalSsid))
       const claimLink = await core.claim(personalSsid, result)
       // TODO the following line should resolve to the same did as above, but it doesnt?
-      //let did = 'did:discipl:ephemeral:'+bsnClaim.ssid.pubkey
-      console.log('service attesting for client did '+did);
+      // let did = 'did:discipl:ephemeral:'+bsnClaim.ssid.pubkey
+      console.log('service attesting for client did ' + did)
       await core.attest(serviceSsid, did, claimLink)
     }, (e) => {
       // If connection is dropped by remote peer, this is fine
@@ -66,23 +62,20 @@ class WaardenpapierenService {
         console.error(e)
       }
     })
-
   }
-
 
   async stop () {
     try {
       await this.ephemeralServer.close()
     } catch (e) {
-      console.log("Error while closing server")
+      console.log('Error while closing server')
       console.log(e)
     }
   }
 
-  getCoreAPI() {
+  getCoreAPI () {
     return abundance.getCoreAPI()
   }
-
 }
 
-export default WaardenpapierenService;
+export default WaardenpapierenService
--- waardepapieren-service/test/index.spec.js
+++ waardepapieren-service/test/index.spec.js
@@ -6,6 +6,7 @@ import WaardenpapierenService from '../src/index'
 import * as abundance from 'discipl-abundance-service'
 
 import { take } from 'rxjs/operators'
+import sinon from 'sinon'
 
 const EPHEMERAL_ENDPOINT = 'http://localhost:3232'
 const EPHEMERAL_WEBSOCKET_ENDPOINT = 'ws://localhost:3233'
@@ -19,8 +20,16 @@ const timeoutPromise = (timeoutMillis) => {
   })
 }
 
-describe('waardenpapieren-service', () => {
+describe('waardenpapieren-service, integrated with mocked nlx connector', () => {
   it('should serve an expressed need for BRP', async () => {
+    let nlxConnector = await abundance.getCoreAPI().getConnector('nlx')
+
+    let nlxClaimStub = sinon.stub(nlxConnector, 'claim').returns('claimId')
+    let nlxGetStub = sinon.stub(nlxConnector, 'get').returns({ 'woonplaats': 'Haarlem' })
+    let nlxConfigureSpy = sinon.spy(nlxConnector, 'configure')
+
+    abundance.getCoreAPI().registerConnector('nlx', nlxConnector)
+
     // Set up server
     let waardenpapierenService = new WaardenpapierenService()
     await timeoutPromise(100)
@@ -28,6 +37,9 @@ describe('waardenpapieren-service', () => {
     await timeoutPromise(100)
     let ephemeralConnector = await abundance.getCoreAPI().getConnector('ephemeral')
     ephemeralConnector.configure(EPHEMERAL_ENDPOINT, EPHEMERAL_WEBSOCKET_ENDPOINT)
+
+    expect(nlxConfigureSpy.callCount).to.equal(1)
+    expect(nlxConfigureSpy.args[0]).to.deep.equal([NLX_OUTWAY_ENDPOINT])
 
     await timeoutPromise(100)
     let needSsid = await abundance.need('ephemeral', BRP_UITTREKSEL)
@@ -38,9 +50,17 @@ describe('waardenpapieren-service', () => {
     await timeoutPromise(100)
     let attestLink = await attestLinkObserver
     await timeoutPromise(100)
+
+    // Test observations
     expect(attestLink.claim.data[needSsid.did]).to.be.a('string')
+
+    expect(nlxClaimStub.callCount).to.equal(1)
+    expect(nlxClaimStub.args[0]).to.deep.equal([null, { 'path': '/haarlem/Basisregistratiepersonen/RaadpleegIngeschrevenPersoonNAW', 'params': { 'burgerservicenummer': '123123123' } }])
+    expect(nlxGetStub.callCount).to.equal(1)
+    expect(nlxGetStub.args[0]).to.deep.equal(['claimId'])
+
     let claim = await abundance.getCoreAPI().get(attestLink.claim.data[needSsid.did])
-    expect(claim.data).to.deep.equal({ woonplaats: 'dummy value' })
+    expect(claim.data).to.deep.equal({ 'woonplaats': 'Haarlem' })
 
     await waardenpapierenService.stop()
   })
