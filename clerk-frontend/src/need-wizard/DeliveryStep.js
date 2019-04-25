77cac93e3e12a99a83d9f49219d6ac534b5afe0d
DennisHouterman <dennishouterman@gmail.com>
2019-04-25 15:39:42 +0200

    Moved canvas drawings to paper wallet repo and added new configuration variables

--- clerk-frontend/src/need-wizard/DeliveryStep.js
+++ clerk-frontend/src/need-wizard/DeliveryStep.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import * as paperWallet from '@discipl/paper-wallet'
-import { loadImage } from 'canvas'
-
 import CONFIGURATION from '../configuration/wpsvc.json'
 
 let template = CONFIGURATION.EXPORT_TYPES['@discipl/paper-wallet'].template
@@ -27,7 +25,6 @@ class ConfirmStep extends Component {
   async componentDidMount() {
     console.log(this.props)
 
-
     let vc = await paperWallet.issue(this.props.resultLink, this.props.myPrivateSsid)
 
     console.log("Issued")
@@ -35,31 +32,7 @@ class ConfirmStep extends Component {
     this.canvasRef.current.width = template.canvasWidth
     this.canvasRef.current.height = template.canvasHeight
 
-
     await paperWallet.toCanvas(vc, template, this.canvasRef.current)
-
-    let context = this.canvasRef.current.getContext('2d')
-    // Draw logo
-    context.drawImage(await loadImage(template.logoImage), template.logoOffsetX, template.logoOffsetY, template.logoWidth, template.logoHeight)
-    // draw discipl logo
-    context.drawImage(await loadImage(template.disciplImage), template.disciplOffsetX, template.disciplOffsetY, template.disciplWidth, template.disciplHeight)
-    // draw product header
-    context.font = template.productHeaderFont
-    context.fillText(template.productHeaderText, template.productHeaderOffsetX, template.productHeaderOffsetY)
-    // draw subheader
-    context.font = template.subheaderFont
-    context.fillText(template.subheaderText, template.subheaderOffsetX, template.subheaderOffsetY)
-    // draw footer
-    context.font = template.footerFont
-    context.fillText("Dit is een automatisch gegenereerd document en daarom niet ondertekend.", 110, 776)
-    context.fillText("De gegevens zijn verkregen via NLX en geborgd in de QR-code", 140, 788)
-    context.fillText("U kunt de echtheid van dit document controleren via een bijbehorende app of online", 85, 800)
-    // draw a line
-    context.beginPath();
-    context.moveTo(40, 250);
-    context.lineTo(570, 250);
-    context.stroke();
-
   }
 
   render() {
