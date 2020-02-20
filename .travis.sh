14e5fa1dad56892a115a0e0340ba20e56162a3c2
Pim Otte <potte@quintor.nl>
2020-02-20 16:51:38 +0100

    Move sourcing of prepare-env script

--- .travis.sh
+++ .travis.sh
@@ -20,7 +20,7 @@ npm audit
 
 # Integration
 cd ..
-./prepare-env.sh
+
 export CERT_HOST_IP=$(ip -o addr show | grep -E "eth0.*inet " | grep -E -o  -e "[0-9]*(\.[0-9]*){3}" | head -1)
 docker-compose -f docker-compose-travis.yml build
 docker-compose -f docker-compose-travis.yml up -d
