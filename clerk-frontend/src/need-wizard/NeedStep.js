c7c8b8c8c5636b1b1d5ec26ea7798e8a4bf81c2c
Datafluisteraar <steven.gort@ictu.nl>
2020-03-20 19:48:46 +0100

    Fix propagation of needs

--- clerk-frontend/src/need-wizard/NeedStep.js
+++ clerk-frontend/src/need-wizard/NeedStep.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 
+import CONFIGURATION from '../configuration/clerk-frontend-config.json'
+const NEEDS = [
+  {
+    'predicate': CONFIGURATION.PRODUCTS[0].PRODUCT_NEED,
+    'description': 'Uittreksel Basis Registratie Persoonsgegevens'
+  },
+  {
+    'predicate': CONFIGURATION.PRODUCTS[1].PRODUCT_NEED,
+    'description': 'Uittreksel Basis Registratie Verblijfplaatshistorie'
+  }
+]
+
 class NeedStep extends Component {
 
   constructor(props) {
     super(props);
-
-    this.needs = [
-      {
-        'predicate': this.props.config.PRODUCT_NEED,
-        'description': 'Uittreksel Basis Registratie Persoonsgegevens'
-      }
-    ]
-
+    this.state = {
+      'currentNeed': NEEDS[0].predicate
+    }   
     if (this.props.needChanged) {
-      this.props.needChanged(this.needs[0].predicate)
+      this.props.needChanged(NEEDS[0].predicate)
+    }
+  }
+
+  needChanged(event) {
+    this.setState({ currentNeed: event.target.value });
+    if (this.props.needChanged) {
+      this.props.needChanged(event.target.value)
     }
   }
 
   renderOptions() {
     let result = []
-    for (let i = 0; i < this.needs.length; i++) {
-      let need = this.needs[i]
+    for (let i = 0; i < NEEDS.length; i++) {
+      let need = NEEDS[i]
       result.push(<option value={need.predicate}>{need.description}</option>)
     }
     return result
@@ -28,13 +42,12 @@ class NeedStep extends Component {
 
   render() {
     return (
-      <div className="input need-form">
-        <label className="input__label" htmlFor="waardepapieren-keuze">
-          Kies uit beschikbare waardepapieren
-        </label>
-        <select id="waardepapieren-keuze" className="input__control input__control--xl input__control--select">
-          {this.renderOptions()}
-        </select>
+      <div className="need-form">
+        <form>
+          <select onChange={this.needChanged.bind(this)} value={this.state.currentNeed}>
+            {this.renderOptions()}
+          </select>
+        </form>
       </div>
     );
   }
