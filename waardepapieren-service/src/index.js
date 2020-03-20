8f00d3fa8464363ae6c416820375075719b078ed
Datafluisteraar <steven.gort@ictu.nl>
2020-03-20 23:38:43 +0100

    Playing with configurationPath for debugging purposes

--- waardepapieren-service/src/index.js
+++ waardepapieren-service/src/index.js
@@ -3,12 +3,11 @@ import WaardepapierenService from './waardepapieren-service'
 
 import fs from 'fs'
 
-let configurationPath = process.env.WAARDEPAPIEREN_CONFIG || '../configuration/waardepapieren-config.json'
+let configurationPath = process.env.WAARDEPAPIEREN_VERBLIJFPLAATSHISTORIE_CONFIG || '../configuration/waardepapieren-verblijfplaatshistorie-config.json'
 
 let configuration = JSON.parse(fs.readFileSync(configurationPath))
 
 console.log('Booting with configuration: ', configuration)
-console.log(configuration)
 
 
 let waardenpapierenService = new WaardepapierenService()
