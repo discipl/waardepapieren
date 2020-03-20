77eafcdca92d13e53ba161d0c0c414ad9245c7da
Datafluisteraar <steven.gort@ictu.nl>
2020-03-20 23:53:18 +0100

    Refactoring configurationPath

--- waardepapieren-service/src/index.js
+++ waardepapieren-service/src/index.js
@@ -3,8 +3,7 @@ import WaardepapierenService from './waardepapieren-service'
 
 import fs from 'fs'
 
-let configurationPath = process.env.WAARDEPAPIEREN_VERBLIJFPLAATSHISTORIE_CONFIG || '../configuration/waardepapieren-verblijfplaatshistorie-config.json'
-
+let configurationPath = process.env.WAARDEPAPIEREN_CONFIG
 let configuration = JSON.parse(fs.readFileSync(configurationPath))
 
 console.log('Booting with configuration: ', configuration)
