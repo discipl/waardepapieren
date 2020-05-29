817f7b1e59f075e8d4063b0e09c8592401b40e9c
Niels van der Veer (n9iels) <n9iels@users.noreply.github.com>
2020-05-29 16:09:02 +0200

    Refactor BSNStep to a functional component

--- clerk-frontend/src/need-wizard/BSNStep.jsx
+++ clerk-frontend/src/need-wizard/BSNStep.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
-class BSNStep extends React.Component {
-  handleChange(event) {
-    if (this.props.bsnChanged) {
-      this.props.bsnChanged(event.target.value)
+const BSNStep = function(props) {
+  function handleBsnChange(event) {
+    if (props.bsnChanged) {
+      props.bsnChanged(event.target.value)
     }
   }
 
-  render() {
-    return (
-      <div className="bsn-form">
-        <form onSubmit={e => e.preventDefault()}>
-          <div className="input">
-            <label className="input__label" htmlFor="waardepapieren-bsn">
-              Voer BSN in
-            </label>
-            <input type="text" id="waardepapieren-bsn" className="input__control input__control--large input__control--text" onChange={e => this.handleChange(e)} />
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="bsn-form">
+      <form onSubmit={e => e.preventDefault()}>
+        <div className="input">
+          <label className="input__label" htmlFor="waardepapieren-bsn">
+            Voer BSN in
+          </label>
+          <input type="text" id="waardepapieren-bsn" className="input__control input__control--large input__control--text" onChange={handleBsnChange} />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default BSNStep;
