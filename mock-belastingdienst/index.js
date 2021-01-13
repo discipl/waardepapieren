af23802e0a773fe371b9c7e791813cd50226fb5a
ZoeMeredith <z.dalmijn@gmail.com>
2021-01-13 13:59:41 +0100

    WIP adding belastingdienst UI

--- mock-belastingdienst/index.js
+++ mock-belastingdienst/index.js
@@ -8,13 +8,6 @@ app.get('/belastingdienst/bsn/999994669', (req, res) => {
             "gegevensbelastingdienst": [
                 {
                     "burgerservicenummer": "999994669",
-                    "naam": {
-                        "voornamen": "Żáïŀëñøŕ Åłéèç Đëļŧå",
-                        "voorletters": "Ż.Å.Đ.",
-                        "geslachtsnaam": "Streeveld",
-                        "voorvoegsel": "'S"
-                    },
-                    "geboortedatum": "10-12-1961",
                     "alimentatieverplichtingen": "geen",
                     "verplichtingenAndersDanBekendBijBKR": "geen",
                     "inkomenUitOnderneming": "geen",
