8179e59594efb161f13ce02c7a6d39d785f0837f
datafluisteraar <steven.gort@ictu.nl>
2021-06-29 12:56:56 +0200

    Jolocom JWT added

--- waardepapieren-component/src/need-wizard/DeliveryStep.jsx
+++ waardepapieren-component/src/need-wizard/DeliveryStep.jsx
@@ -8,7 +8,8 @@ class ConfirmStep extends React.Component {
     this.state = {
       attestationLink : null,
       canvas : null,
-      base64 : null
+      base64 : null,
+      jwt: null
     }
 
     this.template = this.props.config.EXPORT_TYPES['@discipl/paper-wallet'].template
@@ -34,7 +35,8 @@ class ConfirmStep extends React.Component {
     })
 
     this.setState({
-      base64: vc.qr // Contains the base 64 png encoding of the VC QR code
+      base64: vc.qr, // Contains the base 64 png encoding of the VC QR code
+      jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpbnRlcmFjdGlvblRva2VuIjp7ImNhbGxiYWNrVVJMIjoiaHR0cDovLzE5Mi4xNjguMi4yNjo5MDAwL2pvbG8vaW50ZXJ4bi9kNzMwODJkZi04M2Q4LTQ5ODUtYWU2OS05NDMzOTA3MDI0YTMiLCJvZmZlcmVkQ3JlZGVudGlhbHMiOlt7InR5cGUiOiJQcm9vZk9mSWRDcmVkZW50aWFsRGVtbyIsInJlbmRlckluZm8iOnsicmVuZGVyQXMiOiJkb2N1bWVudCJ9LCJjcmVkZW50aWFsIjp7Im5hbWUiOiJTU0kgTmF0aW9uYWFsIFNwb29yIC0gVXNlIENhc2UgQmVzbGFndnJpamUgVm9ldCIsImRpc3BsYXkiOnsicHJvcGVydGllcyI6W3sicGF0aCI6WyIkLkJlc2xhZ3ZyaWplIHZvZXQiXSwibGFiZWwiOiJCZWxhc3RpbmdkaWVuc3QiLCJ2YWx1ZSI6IjE1NjMifSx7InBhdGgiOlsiJC5DRFciXSwibGFiZWwiOiJHZXJlY2h0c2RldXJ3YWFyZGVyIiwidmFsdWUiOiJTYW5uZSBWb29yc3BvZWQifSx7InBhdGgiOlsiJC5JbmtvbWVuc3ZlcmhvdWRpbmciXSwibGFiZWwiOiJVV1YiLCJ2YWx1ZSI6IjE4NDYifV19fX1dfSwidHlwIjoiY3JlZGVudGlhbE9mZmVyUmVxdWVzdCIsImlhdCI6MTYyNDk2MzY2MTQ2MiwiZXhwIjoxNjI0OTY3MjYxNDYyLCJqdGkiOiJmYzUxMzc2ODEyMDQyOTk3IiwiaXNzIjoiZGlkOmpvbG86MDhjNGRhZDdlZDdkNTVlMjViNzJlNzVhZWFkNjkyMGJiZjYzYThjMWI4Yjg3Zjc5ZDhlNWMyYjg0MjdmMWY5ZiNrZXlzLTEifQ.nqxDD4QgABxp2TtljN9-Jkm1X-nsJw9tGuI5w22opXFUTHydh7NK-KomWe9KwBFQTXlLh9Fk8_bzef-WJWox1g"
     })
 
     console.log("Issued")
@@ -63,6 +65,8 @@ class ConfirmStep extends React.Component {
         <canvas id="delivery-canvas" className="responsive" ref={this.canvasRef} />
         <p>Base64 encoding of the VC QR code is:</p>
         <img src={this.state.base64}></img>
+        <p>Jolocom JWT:</p>
+        <img src={this.state.jwt}></img>
       </div>
     );
   }
